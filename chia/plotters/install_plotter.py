f516d37663da044445181df39a11717bc8c497fb
ChiaMineJP <admin@chiamine.jp>
2022-04-28 00:44:26 +0900

    Implemented `chia plotters bladebit2` and handling for its options

--- chia/plotters/install_plotter.py
+++ chia/plotters/install_plotter.py
@@ -20,7 +20,13 @@ def install_plotter(args: Namespace, root_path: Path):
         return
     elif plotter == "bladebit":
         try:
-            install_bladebit(root_path, override, commit)
+            install_bladebit(root_path, override, commit or "ad85a8f2cf99ca4c757932a21d937fdc9c7ae0ef")
+        except Exception as e:
+            print(f"Exception while installing bladebit plotter: {e}")
+        return
+    elif plotter == "bladebit2":
+        try:
+            install_bladebit(root_path, override, commit or "disk_plot")
         except Exception as e:
             print(f"Exception while installing bladebit plotter: {e}")
         return
